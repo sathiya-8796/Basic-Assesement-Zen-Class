fc8b59debb3268e1bd6b0ea391245815f6b170e5
sathiya-8796 <67754729+sathiya-8796@users.noreply.github.com>
2020-07-30 08:39:47 +0530

    Update Scripts.js

--- 26-July-2020/CORS/Scripts.js
+++ 26-July-2020/CORS/Scripts.js
@@ -1,13 +1,15 @@
+var url_string = 'https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com';
+var proxy = 'https://cors-anywhere.herokuapp.com/'
 
 
-
-
-
-
-var url_string =  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e23c7ea952d3f17241e3047b473b9282"
-fetch(url_string) // Call the fetch function passing the url of the API as a parameter
-//console.log(url_string2)
+fetch(proxy + url_string)
 .then(res=>res.json())
 .then(data=>console.log(data))
+
+
+
+
+
+
 
 
